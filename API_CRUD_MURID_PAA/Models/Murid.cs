d2c32854743d58cac3fc48a3f2152c11079c192f
tunggulalmajid <tunggulmajid@gmail.com>
2026-04-01 12:07:31 +0700

    Update : FIx

--- API_CRUD_MURID_PAA/Models/Murid.cs
+++ API_CRUD_MURID_PAA/Models/Murid.cs
@@ -7,6 +7,7 @@ namespace API_CRUD_MURID_PAA.Models
         public int idMurid { get; set; }
         public string nama { get; set; }
         public DateOnly tanggalLahir { get; set; }
+        public string kelas {  get; set; }
         public string email { get; set; }
         public string alamat { get; set; }
         
--- API_CRUD_MURID_PAA/Models/MuridContext.cs
+++ API_CRUD_MURID_PAA/Models/MuridContext.cs
@@ -17,8 +17,8 @@ namespace API_CRUD_MURID_PAA.Models
             string query = string.Format(@"select * from murids order by id_murid");
             try
             {
-                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
-                NpgsqlDataReader reader = cmd.ExecuteReader();
+                using NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                using NpgsqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
                     ListMurid.Add(new Murid()
@@ -26,11 +26,11 @@ namespace API_CRUD_MURID_PAA.Models
                         idMurid = int.Parse(reader["id_murid"].ToString()),
                         nama = reader["nama"].ToString(),
                         tanggalLahir = DateOnly.Parse(reader["tanggal_lahir"].ToString()),
+                        kelas = reader["kelas"].ToString(),
                         email = reader["email"].ToString(),
                         alamat = reader["alamat"].ToString()
                     });
                 }
-                cmd.Dispose();
                 db.closeConnection();
             }
             catch (Exception ex) {
@@ -42,17 +42,17 @@ namespace API_CRUD_MURID_PAA.Models
 
         public void TambahMurid(Murid murid)
         {
-            string query = string.Format(@"insert into murids(nama, tanggal_lahir, email, alamat) 
-                    values (@nama, @tanggal_lahir, @email, @alamat)");
+            string query = string.Format(@"insert into murids(nama, tanggal_lahir, kelas,  email, alamat) 
+                    values (@nama, @tanggal_lahir, @kelas, @email, @alamat)");
 
             try
             {
-                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                using NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
                 cmd.Parameters.AddWithValue("@nama", murid.nama);
                 cmd.Parameters.AddWithValue("@tanggal_lahir", murid.tanggalLahir);
+                cmd.Parameters.AddWithValue("@kelas", murid.kelas);
                 cmd.Parameters.AddWithValue("@email", murid.email);
                 cmd.Parameters.AddWithValue("@alamat", murid.alamat);
-
                 cmd.ExecuteNonQuery();
                 db.closeConnection();
             }
@@ -65,7 +65,7 @@ namespace API_CRUD_MURID_PAA.Models
         public void UpdateMurid(Murid murid) 
         {
             string query = string.Format(@"UPDATE murids 
-                     SET nama = @nama, tanggal_lahir = @tanggal_lahir, email = @email, alamat = @alamat 
+                     SET nama = @nama, tanggal_lahir = @tanggal_lahir, kelas = @kelas ,email = @email, alamat = @alamat 
                      WHERE id_murid = @id");
 
             try
@@ -74,6 +74,7 @@ namespace API_CRUD_MURID_PAA.Models
                 cmd.Parameters.AddWithValue("@id", murid.idMurid);
                 cmd.Parameters.AddWithValue("@nama", murid.nama);
                 cmd.Parameters.AddWithValue("@tanggal_lahir", murid.tanggalLahir);
+                cmd.Parameters.AddWithValue("@kelas", murid.kelas);
                 cmd.Parameters.AddWithValue("@email", murid.email);
                 cmd.Parameters.AddWithValue("@alamat", murid.alamat);
 
